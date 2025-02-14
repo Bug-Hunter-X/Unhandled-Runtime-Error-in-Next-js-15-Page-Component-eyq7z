a108b87d34e258a7362cb3a3a2a6c16ca93244c6
BugHunter-X <askzenai@gmail.com>
2025-02-14 04:14:05 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,28 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause the error
+  console.log(unexist);
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+```
